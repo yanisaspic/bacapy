b2b44d5756b82456918a945084e5b9e38e99f7c0
acolajanni <73993119+acolajanni@users.noreply.github.com>
2022-01-22 15:29:36 +0100

    final

--- pathway_expr.py
+++ pathway_expr.py
@@ -5,16 +5,20 @@ from aggregate_expression import *
 from heatmap import *
 
 
+
+
 def get_expr_pathway(graph, method = "mean"):
     """
-    Créer un dictionnaire rassemblant les données d'expression 
-    des réactions contenus dans chaque pathway
+    Créer un dictionnaire rassemblant les données d'expression en liste 
+    de liste des réactions contenus dans chaque pathway
     
     Paramètres
     ----------
     graph : tlp.Graph
     method : string parmi ['mean', 'maxStd', 'minStd', 'upDownZ', 'normalZ']
-    
+
+    ----------
+    Return : dictionnaire python
     """
     path_expr = {}
     for subgraph in graph.getSubGraphs():
@@ -34,13 +38,15 @@ def get_expr_pathway(graph, method = "mean"):
 
 def fill_na(path_expr, tp = 17):
     """
-    Rempli les pathways d'une liste de 0 si ils contiennent aucune expression
+    Rempli les pathways d'une liste de "NA" si ils ne contiennent aucune expression
     
     Paramètres
     ----------
-    graph : tlp.Graph
+    path_expr : tlp.Graph
     tp : int, nombre de timestamp
     
+    ----------
+    Return : pandas.Dataframe
     """
     None_list = ["NA"]*tp
     for i in path_expr : 
@@ -50,99 +56,54 @@ def fill_na(path_expr, tp = 17):
              
 def get_aggregated_expr_pathway(graph, tp = 17, method="mean"):
     """
-    Créer un dictionnaire rassemblant les données d'expression 
-    des réactions contenus dans chaque pathway et les aggrègent
+    Créer un dataframe qui aggrègent les données d'expression de réaction
+    par pathway
     
     Paramètres
     ----------
     graph : tlp.Graph
+    tp : int, nombre de timestamp
     method : string parmi ['mean', 'maxStd', 'minStd', 'upDownZ', 'normalZ']
     
+    ----------
+    Return : pandas.Dataframe
     """
     
     path_expr = get_expr_pathway(graph, method)
     path_expr = fill_na(path_expr, tp)
     
-    
     pathway_expression = pd.DataFrame.from_dict(path_expr).T
     tp = "tp"
     col = []
-    #for i in range(1,18):
-    [col.append( tp+str(i) ) for i in range(1,18)]
+    [col.append( tp+str(i) ) for i in range(0,17)]
     pathway_expression.columns = col     
     return(pathway_expression)
-         
+
              
 def main(graph):
     wg=graph.getSubGraph("Workingraph")
     quotient=graph.getSubGraph("quotient of Workingraph")
     
-    pathway_expression = get_aggregated_expr_pathway(wg)
+    pathway_expression = get_aggregated_expr_pathway(wg, method = "mean")
     
-    heat = graph.getSubGraph("heatmap")
-    if heat == None :
-        heat = graph.addSubGraph("heatmap")
-    else : 
-        heat.clear()
-        
-    heat = tlp.newGraph()
-    heat.setName("heatmap")
-    
-   
-    
-    print(tlpgui.getViewsOfGraph(heat))
+    roots = tlp.getRootGraphs()
+    heat = None
+    has_a_view = False
+    for gr in roots :
+       if gr.getName() == "heatmap" :
+           heat=gr
+           heat.clear()
+           has_a_view = True
 
-    heatmap(heat, pathway_expression, "expr")  
-    prop = tlp.DataSet()
-    view = tlpgui.getViewsOfGraph(heat)    
-    print(tlpgui.getViewsOfGraph(graph))
-    heat = tlpgui.createView("Node Link Diagram view",heat,prop,True)  
-    
-    
-    
-    
-    #heat.delLocalProperty("viewFont")
-    #print(font)
-    #print(pathway_expression)
-    
-    """
-    path_expr = {}
-    for subgraph in wg.getSubGraphs():
-        pathway = subgraph.getName() 
-        
-        expr_list = []
-        for node in subgraph.getNodes():
-            expr = subgraph["expression"][node]   
-            if expr != []  :
-                expr_list.append(expr)
-        
-        expr_df = pd.DataFrame.from_records(expr_list)
-        aggregated_expr = getDataFrameRow(expr_df, "mean")
+    if heat == None : 
+        heat = tlp.newGraph()
+        heat.setName("heatmap")
 
-        path_expr[pathway] = aggregated_expr.to_list()
-    
-    
-    None_list = [0]*17
-    for i in path_expr : 
-        if len(path_expr[i]) == 0:
-             path_expr[i] = None_list
-    
-    pathway_expression = pd.DataFrame.from_dict(path_expr).T
-    tp = "tp"
-    col = []
-    for i in range(1,18):
-        col.append( tp+str(i) )
-    pathway_expression.columns = col
-    
+    df = cluster_row(pathway_expression)
 
-    """
+    heatmap(heat, df, "expr", True) 
     
-
-    
-
-    """
-    for path in path_expr :
-        print("\n Pathway :",path, "nombre de gènes :", len(path_expr[path]) )
-        for i in path_expr[path]:
-            print(i)
-    """
+    if not has_a_view : 
+        prop = tlp.DataSet()
+        #view = tlpgui.getViewsOfGraph(heat)    
+        heat = tlpgui.createView("Node Link Diagram view",heat,prop,True)  
