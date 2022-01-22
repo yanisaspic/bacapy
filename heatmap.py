f670517f0882c76c2c3cf948d80554a8d3a8dbca
acolajanni <73993119+acolajanni@users.noreply.github.com>
2022-01-22 15:46:57 +0100

    heatmap fini

--- heatmap.py
+++ heatmap.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from tulip import tlp
+from tulipgui import tlpgui
 
 import scipy.cluster.hierarchy as spc
 from handle_files import *
@@ -9,13 +10,18 @@ from handle_files import *
 def get_nodes(gr, lignes=2, colonnes=2, nodes = {}):
     """
     Créer autant de nodes que nécessaire pour la heatmap
+    Les stocke dans un dictionnaire indicé par leur 
+    numéro de ligne et colonne
     
     Paramètres
     ----------
     gr : tlp.Graph
     lignes : int 
     colonnes : int
-    nodes : python dictionnaire 
+    nodes : dictionnaire python
+    
+    ----------
+    Return : dictionnaire python
     
     """
     for i in range(lignes) : 
@@ -26,9 +32,9 @@ def get_nodes(gr, lignes=2, colonnes=2, nodes = {}):
     
     return nodes
    
-def grid_map(nodes, lignes, colonnes, graph  ):
+def grid_map(nodes, lignes, colonnes, graph):
     """
-    Ordonne les noeuds pour créer une heatmap
+    Ordonne les noeuds pour créer une grille
     
     Paramètres
     ----------
@@ -43,15 +49,11 @@ def grid_map(nodes, lignes, colonnes, graph  ):
     for i in range(colonnes):   
         for j in range(lignes):
 
-            # On place les noeud de la première colonne plus à gauche : nom des lignes
-            #if i == 0 :
-             #   graph['viewLayout'][nodes[j][i]] = (i-3,-j,0)
-            #else :
             graph['viewLayout'][nodes[j][i]] = (i,-j,0)
 
 def add_property(graph,df, nodes, property_name="expr") :
     """
-    Ajoute une propriété (type int/double) 
+    Ajoute une propriété (type double) 
     permettant de colorer les cases de la heatmap
     
     
@@ -76,19 +78,17 @@ def add_property(graph,df, nodes, property_name="expr") :
 
 def color_heatmap(graph, prop, nodes):
     """
-    Colore la heatmap en fonction de la propriété donnée en paramètres
+    Colore la heatmap en fonction de la propriété donnée en paramètre
     
     Paramètres
     ----------
     graph : tlp.Graph
     prop : nom de la propriété du graph à mapper sur les noeuds 
-    nodes : python dictionnaire 
+    nodes : dictionnaire python
     
     """
-    # dict qui récup les paramètres
     params = tlp.getDefaultPluginParameters("Color Mapping", graph)
-    
-    # On map la propriété 'prop' dans à la case ('property')
+
     params['property'] = graph[prop]
     
     colorScale = tlp.ColorScale([])    
@@ -102,7 +102,7 @@ def color_heatmap(graph, prop, nodes):
 
 def add_scale(graph, df, prop):
     """
-    Ajoute une échelle de couleur
+    Ajoute une échelle de couleur à la heatmap
     
     Paramètres
     ----------
@@ -113,21 +113,20 @@ def add_scale(graph, df, prop):
     """
     col = df.shape[1]
     
-    global_max = int(max(df.max().tolist()[1:]))
-    global_min = int(min(df.min().tolist()[1:]))
-    
-    for i in range(global_min, global_max+1, 1):
-        #print(i)        
+    global_max = int(max(df.max().tolist())*10)
+    global_min = int(min(df.min().tolist())*10)
+        
+    for i in range(global_min, global_max+1, 10):        
         node = graph.addNode()
-        graph['viewLayout'][node] = (col+2, i-4, 0)
-        graph[prop][node] = int(i)
-        graph['viewLabel'][node] = str(i)
-        # position du label : gauche
-        graph['viewLabelPosition'][node] = 3
+        graph['viewLayout'][node] = (col+2, (i-40)/10, 0)
+        graph[prop][node] = i/10
+        graph['viewLabel'][node] = str(i/10)
+        graph['viewLabelPosition'][node] = tlp.LabelPosition.Left
+        graph['viewFontSize'][node] = 14
         
 def add_features(graph, df, feature) :
     """
-    Ajoute les nomes des colonnes ou des lignes à la heatmap
+    Ajoute les noms des colonnes ou des lignes à la heatmap
     
     Paramètres
     ----------
@@ -136,7 +135,6 @@ def add_features(graph, df, feature) :
     feature : str ["header","index"] 
 
     """
-    
     if feature == "header" :
         ft = df.columns
         
@@ -147,28 +145,83 @@ def add_features(graph, df, feature) :
     for col in ft :
         
         node = graph.addNode()
-    #    graph['viewFont'][node] = "C:/Users/Antonin Colajanni/Desktop/M2/R_BOURQUI/bacapy/OpenSans-Regular.ttf"
-        #graph['viewFont'][node] =  
         graph['viewLabel'][node] = col
         graph['viewFontSize'][node] = 18
-        #graph['viewLabelPosition'][node] = 4
-
-        #graph['viewLabelPosition'][node] = 1
+        graph['viewShape'][node] = tlp.NodeShape.CubeOutlinedTransparent
         graph['viewColor'][node] = tlp.Color.White
-        
                   
         if feature == "header" :
             graph['viewLayout'][node] = (count, 1, 0)
+            graph['viewSize'][node] = ( (0.2 ,0.2 ,0) )
             graph['viewRotation'][node] = 90
+            graph['viewLabelPosition'][node] = tlp.LabelPosition.Right
                     
         if feature == "index" :
-            graph['viewLayout'][node] = (-3, -count, 0)
-            graph['viewSize'][node] = ( (0.9 ,4 ,0) )
+            graph['viewLayout'][node] = (-0.5, -count, 0)
+            graph['viewSize'][node] = ( (0.1 ,0.9 ,0) )
+            graph['viewLabelPosition'][node] = tlp.LabelPosition.Left
 
         count +=1
 
+def remove_NA(df,col="tp1") : 
+    """
+    Enlève les lignes d'un dataframe contenant "NA"
+    
+    ----------
+    Paramètres
+    df : pandas.DataFrame
+    col : str, nom de colonne
+    
+    ----------
+    Return : pandas.DataFrame
+    """
+    return df.loc[df[col] != "NA"]
+    
+def get_corr_mat(df):
+    """
+    Créer une matrice de corrélation par 
+    paire entre les lignes du dataframe
+    
+    ----------
+    Paramètres
+    df : pandas.DataFrame
+    col : str, nom de colonne
+    
+    ----------
+    Return : pandas.DataFrame
+    """
+    df = remove_NA(df)
+    transposed = df.T
+    transposed.columns = df.index
+    transposed = transposed.astype("float64")    
+    return transposed.corr()
+    
+def cluster_row(df):
+    """
+    Ordonne les colonne d'un Dataframe en fonction de la 
+    corrélation entre les lignes
+    
+    
+    ----------
+    Paramètres
+    df : pandas.DataFrame
+    
+    ----------
+    Return : pandas.DataFrame
+    """
 
-def heatmap(graph, df, property_name) :
+    df = remove_NA(df)
+    corr_matrix = get_corr_mat(df)
+    pdist = spc.distance.pdist(corr_matrix)
+    linkage = spc.linkage(pdist, method='ward' )
+    idx = spc.fcluster(linkage, 0.5 * pdist.max(), 'distance')
+    df['clust'] = idx
+    df_clust = df.sort_values(by=['clust'], )
+    df = df_clust.drop(columns = 'clust', axis = 1)
+    return df 
+
+
+def heatmap(graph, df, property_name, cluster = True) :
     """
     Affiche une heatmap à partir d'un pandas.DataFrame
     
@@ -177,61 +230,20 @@ def heatmap(graph, df, property_name) :
     graph : tlp.Graph
     df : pandas.DataFrame
     prop : nom de la propriété du graph à mapper sur les noeuds 
+    cluster : Booleen, True : Ordonne les lignes de la heatmap
 
     """
     
     lignes = df.shape[0]
     col = df.shape[1]
     nodes = get_nodes(graph, lignes, col)
+    
+    if cluster :
+        df = cluster_row(df)
+    
     add_property(graph, df, nodes,  property_name)
     grid_map(nodes, lignes, col, graph)
     add_scale(graph, df, property_name)
     color_heatmap(graph, property_name, nodes)
     add_features(graph, df, "header")
     add_features(graph, df, "index")
-    
-#def cluster_row():
-    
-
-### Test
-def main(graph):
-    
-    #graph.clear()
-    
-
-
-    genes, levels, ratios = loadDataFiles()
-
-    ratio = ratios.iloc[30:130, :]
-    lignes = ratio.shape[0]
-    col = ratio.shape[1]
-
-    
-    """
-    genes = ratio['locus']  
-    rownames = ratio.locus.to_list()
-    ratio = ratio.drop(axis=1, columns= "locus")
-    ratio.index = rownames  
-    
-    
-    transposed = ratio.T
-    transposed.columns = genes
-    transposed = transposed.drop(axis=0, index = 'locus')
-    transposed = transposed.astype("float64")    
-    corr_matrix = transposed.corr()
-    
-    pdist = spc.distance.pdist(corr_matrix)
-    
-    linkage = spc.linkage(pdist, method='ward' )
-    idx = spc.fcluster(linkage, 0.5 * pdist.max(), 'distance')
-    
-    ratio['clust'] = idx
-    ratio_clust = ratio.sort_values(by=['clust'], )
-    ratio = ratio_clust.drop(columns = 'clust', axis = 1)
-    
-
-    heatmap(graph, ratio,  "aaa")
-    """
-
-
-      
